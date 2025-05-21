6a54c0d22f0e1af38d5cf75fb69093e96bddcc6a
quocbahuynh <quochbcontact@gmail.com>
2025-05-21 21:03:15 +0700

    add plus controller

--- src/main/java/com/example/springmvc/GreetingController.java
+++ src/main/java/com/example/springmvc/GreetingController.java
@@ -3,20 +3,20 @@ package com.example.springmvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
 	
-	@GetMapping("/hello-quoc")
-	public String greetingQuoc(Model model) {
-		model.addAttribute("name", "Quốc");
+	@GetMapping("/plus")
+	public String search(Model model, @RequestParam(name = "first", defaultValue = "10") int a, @RequestParam(name = "secound", defaultValue = "1") int b) {
+		int result = a + b;
+		
+		model.addAttribute("firstNumber", a);
+		model.addAttribute("secoundNumber", b);
+		model.addAttribute("result", result);
 		return "greeting";
 	}
 	
-	@GetMapping("/hello-xuan")
-	public String greetingXuan(Model model) {
-		model.addAttribute("name", "Xuan");
-		return "greeting";
-	}
-	
+
 }
